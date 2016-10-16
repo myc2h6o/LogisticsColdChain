73b91bec886f2b1075bc002588000f26dc8235b2
mycspring <1248036495@qq.com>
2016-10-16 15:24:28 +0800

    UI project controller init.

---
+++ Platform/UI/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿namespace UI.Controllers
+{
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.title = "Logistics Platform";
+            return View();
+        }
+    }
+}
