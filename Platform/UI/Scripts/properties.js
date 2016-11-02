52826a914958d43bb14b9fae8a953acd46918a88
mycspring <1248036495@qq.com>
2016-11-02 20:05:24 +0800

    Update front ui.

--- Platform/UI/Scripts/properties.js
+++ Platform/UI/Scripts/properties.js
@@ -12,21 +12,34 @@ var Property = {
     TYPE: "Type",
     NumberMin: "Min",
     NumberMax: "Max",
-    tableNames: ['Cold_Storage', 'Cold_Storage_Inventory', 'Customer', 'Distribution', 'Refrigerator_Car', 'Supplier', 'Vehicle_Status'],
+    tableNames: ['Cars', 'Cars_Status', 'Cold_Storage_Inventories', 'Cold_Storages', 'Customers', 'Orders', 'Suppliers'],
     tableDisplayNames: ['Cold Storage', 'Cold Storage Inventory', 'Customer', 'Distribution', 'Refrigerator Car', 'Supplier', 'Vehicle Status'],
-    Cold_Storage: ['Cold_Storage_Number', 'Cold_Storage_Address', 'Cold_Storage_Scale', 'Cold_Storage_Company'],
-    Cold_StorageDisplay: ['Number', 'Address', 'Scale', 'Company'],
-    Cold_Storage_Inventory: ['Cold_Storage_Number', 'Inventory_Rate', 'Commodity', 'Quantity'],
-    Cold_Storage_InventoryDisplay: ['Cold Storage Number', 'Inventory Rate', 'Commodity', 'Quantity'],
-    Customer: ['Customer_Number', 'Customer1', 'Customer_Address', 'Phone_Number'],
-    CustomerDisplay: ['Number', 'Customer', 'Address', 'Phone'],
-    Distribution: ['Distribution_Number', 'Supplier_Number', 'Customer_Number', 'Supplier_Cold_Storage_Number', 'Customer_Cold_Storage_Number', 'Commodity', 'Quantity', 'Distribution_Status', 'Distribution_Vehicle', 'Deadline', 'Departure_Time', 'Arrival_Time'],
-    DistributionDisplay: ['Number', 'Supplier Number', 'Customer Number', 'Supplier Cold Storage Number', 'Customer Cold Storage Number', 'Commodity', 'Quantity', 'Distribution Status', 'Distribution Vehicle', 'Deadline', 'Departure Time', 'Arrival Time'],
-    Refrigerator_Car: ['License_Plate_Number', 'Vehicle_Type', 'Vehicle_Load', 'Fuel_Consumption', 'Driver', 'Phone_Number', 'Minimum_Temperature', 'Maximum_Temperature', 'Company', 'Car_Register'],
-    Refrigerator_CarDisplay: ['License Plate', 'Type', 'Load', 'Fuel Consumption', 'Driver', 'Phone', 'Minimum Temperature', 'Maximum Temperature', 'Company', 'Car Register'],
-    Supplier: ['Supplier_Number', 'Supplier1', 'Supplier_Address', 'Phone_Number'],
-    SupplierDisplay: ['Number', 'Supplier', 'Address', 'Phone'],
-    SupplierType: [PropertyType.STRING, PropertyType.STRING, PropertyType.STRING, PropertyType.NUMBER],
-    Vehicle_Status: ['License_Plate_Number', 'Vehicle_Status1', 'Setting_Temperature', 'Realtime_Temperature', 'Realtime_Position'],
-    Vehicle_StatusDisplay: ['License Plate', 'Status', 'Setting Temperature', 'Realtime Temperature', 'Realtime Position']
+
+    Cars: ['License_Plate_Number', 'Tonnage', 'Load_Bearing', 'Fuel_Consumption', 'Driver', 'Phone_Number', 'Minimum_Temperature', 'Maximum_Temperature', 'Company', 'Register_Place'],
+    CarsDisplay: ['License Plate Number', 'Tonnage', 'Load Bearing', 'Fuel Consumption', 'Driver', 'Phone Number', 'Minimum Temperature', 'Maximum Temperature', 'Company', 'Register Place'],
+    CarsType: [PropertyType.STRING, PropertyType.NUMBER, PropertyType.NUMBER, PropertyType.NUMBER, PropertyType.STRING, PropertyType.STRING, PropertyType.NUMBER, PropertyType.NUMBER, PropertyType.STRING, PropertyType.STRING],
+
+    Cars_Status: ['License_Plate_Number', 'Status', 'Setting_Temperature', 'Realtime_Temperature', 'Realtime_Position'],
+    Cars_StatusDisplay: ['License Plate Number', 'Status', 'Setting Temperature', 'Realtime Temperature', 'Realtime Position'],
+    Cars_StatusType: [PropertyType.STRING, PropertyType.STRING, PropertyType.NUMBER, PropertyType.NUMBER, PropertyType.STRING],
+
+    Cold_Storage_Inventories: ['Number', 'Usage_Rate', 'Commodity', 'Quantity'],
+    Cold_Storage_InventoriesDisplay: ['Number', 'Usage Rate', 'Commodity', 'Quantity'],
+    Cold_Storage_InventoriesType: [PropertyType.STRING, PropertyType.NUMBER, PropertyType.STRING, PropertyType.NUMBER],
+
+    Cold_Storages: ['Number', 'Address', 'Scale', 'Company'],
+    Cold_StoragesDisplay: ['Number', 'Address', 'Scale', 'Company'],
+    Cold_StoragesType: [PropertyType.STRING, PropertyType.STRING, PropertyType.NUMBER, PropertyType.STRING],
+
+    Customers: ['Number', 'CName', 'Address', 'Phone_Number'],
+    CustomersDisplay: ['Number', 'CName', 'Address', 'Phone Number'],
+    CustomersType: [PropertyType.STRING, PropertyType.STRING, PropertyType.STRING, PropertyType.STRING],
+
+    Orders: ['Number', 'Supplier_Number', 'Customer_Number', 'Supplier_Cold_Storage_Number', 'Customer_Cold_Storage_Number', 'Commodity', 'Quantity', 'Status', 'Car_Assigned', 'Deadline', 'Departure_Time', 'Arrival_Time'],
+    OrdersDisplay: ['Number', 'Supplier Number', 'Customer Number', 'Supplier Cold Storage Number', 'Customer Cold Storage Number', 'Commodity', 'Quantity', 'Status', 'Car Assigned', 'Deadline', 'Departure Time', 'Arrival Time'],
+    OrdersType: [PropertyType.STRING, PropertyType.STRING, PropertyType.STRING, PropertyType.STRING, PropertyType.STRING, PropertyType.STRING, PropertyType.NUMBER, PropertyType.STRING, PropertyType.STRING, PropertyType.STRING, PropertyType.STRING, PropertyType.STRING],
+
+    Suppliers: ['Number', 'SName', 'Address', 'Phone_Number'],
+    SuppliersDisplay: ['Number', 'SName', 'Address', 'Phone Number'],
+    SuppliersType: [PropertyType.STRING, PropertyType.STRING, PropertyType.STRING, PropertyType.STRING]
 }
